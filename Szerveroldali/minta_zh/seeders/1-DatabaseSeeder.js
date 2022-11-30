e87b0259108051846db763bc4805063345057cc0
ogike <tasi.zoltan07@gmail.com>
2022-11-30 12:59:42 +0100

    web mintazh

--- Szerveroldali/minta_zh/seeders/1-DatabaseSeeder.js
+++ Szerveroldali/minta_zh/seeders/1-DatabaseSeeder.js
@@ -1,15 +1,91 @@
 "use strict";
 
+//generáló CLI commandok:
+// npx sequelize model:generate --name User --attributes "email:string"
+// npx sequelize model:generate --name Playlist --attributes "title:string,private:boolean,UserId:integer"
+// npx sequelize model:generate --name Track --attributes "title:string,length:integer,author:string,genres:string,album:string,url:string"
+
+// npx sequelize migration:generate --name create-playlist-track
+
 // Faker dokumentáció, API referencia: https://fakerjs.dev/guide/#node-js
 const { faker } = require("@faker-js/faker");
 const chalk = require("chalk");
-//const { ... } = require("../models");
+const { Track, Playlist, User } = require("../models");
+const slug = require('slug');
 
 module.exports = {
     up: async (queryInterface, Sequelize) => {
         try {
             // Ide dolgozd ki a seeder tartalmát:
-            // ...
+
+            const tracks = [];
+            const tracksCount = faker.datatype.number({
+                min: 20,
+                max: 30,
+            });
+
+            for (let i = 0; i < tracksCount; i++) {
+                // title: string (zeneszám címe)
+                // length: integer (zeneszám hossza másodpercekben)
+                // author: string, nullable (zeneszám szerzője)
+                // genres: string, nullable (zeneszám műfajai, egyszerűen szövegként)
+                // album: string, nullable (melyik albumba tartozik a szám - csak szövegként)
+                // url: string (zeneszámra mutató hivatkozás, pl. Spotify link, stb)
+                const title = faker.music.songName();
+
+                tracks.push(
+                    await Track.create({
+                        title, // ugyanaz, mint a title: title
+                        length: faker.datatype.number({
+                            min: 60,
+                            max: 360,
+                        }),
+                        author: faker.name.fullName(),
+                        // genres: faker.music.genre(),
+                        genres: new Array(
+                            faker.datatype.number({
+                                min: 1,
+                                max: 3,
+                            })
+                        ).fill(1).map(e => faker.music.genre()).join(', '),
+                        album: faker.lorem.word(),
+                        url: `https://szerveroldali-webprog-zenetar.com/${faker.datatype.number({
+                            min: 1000,
+                            max: 9999,
+                        })}-${slug(title)}`
+                    })
+                );
+            }
+
+            const usersCount = faker.datatype.number({
+                min: 5,
+                max: 10,
+            });
+
+            for (let i = 0; i < usersCount; i++) {
+                const user = await User.create({
+                    // vagy: https://fakerjs.dev/api/helpers.html#unique
+                    email: `user${i+1}@szerveroldali.hu`,
+                });
+
+                // Minden userhez generálunk playlisteket
+                const playlistsCount = faker.datatype.number({
+                    min: 0,
+                    max: 4,
+                });
+
+                for (let j = 0; j < playlistsCount; j++) {
+                    const playlist = await user.createPlaylist({
+                        title: faker.lorem.word(),
+                        private: Math.random() < 0.3
+                    });
+
+                    await playlist.setTracks(
+                        faker.helpers.arrayElements(tracks)
+                    );
+                }
+
+            }
 
             console.log(chalk.green("A DatabaseSeeder lefutott"));
         } catch (e) {
